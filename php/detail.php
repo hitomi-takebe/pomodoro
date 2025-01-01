d23244ace8359bb84b7afd2b916f60809345351a
hitomi-takebe <takebe.hitomi@gmail.com>
2025-01-01 23:18:21 +0900

    select12

--- php/detail.php
+++ php/detail.php
@@ -29,9 +29,9 @@ if ($status === false) {
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/style.css">
     <style>
-        div {
-            padding: 10px;
-            font-size: 16px;
+        .submit {
+            max-width: 300px;
+            margin-bottom: 20px;
         }
     </style>
 </head>
@@ -40,7 +40,7 @@ if ($status === false) {
     <header>
         <nav class="navbar navbar-default">
             <div class="container-fluid">
-                <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+                <div class="submit"><a class="navbar-brand" href="select.php">データ一覧</a></div>
             </div>
         </nav>
     </header>
@@ -55,7 +55,7 @@ if ($status === false) {
                 <label>次回：<input type="text" name="next" value="<?= $result['next'] ?>"></label><br>
                 <!-- <label><textarea name="content" rows="4" cols="40"><?= $result['name'] ?></textarea></label><br> -->
                 <input type="hidden" name="id" value="<?= $result['id'] ?>">
-                <input type="submit" value="送信">
+                <input type="submit" class="submit" value="送信">
             </fieldset>
         </div>
     </form>
