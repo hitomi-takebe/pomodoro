7fd628f5413fe42a3af668bb2d17955c0f66a6c6
hitomi-takebe <takebe.hitomi@gmail.com>
2025-01-01 18:40:49 +0900

    selectphp

--- php/select.php
+++ php/select.php
@@ -32,13 +32,11 @@ if ($status === false) {
 <html lang="ja">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ポモドーロアプリ</title>
-    <link rel="stylesheet" href="..css/reset.css">
-    <link rel="stylesheet" href="..css/style.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
     <style>
         div {
             padding: 10px;
