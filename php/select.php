5b4f8e47840468c5b5d487824b1ab55cf2384d08
hitomi-takebe <takebe.hitomi@gmail.com>
2025-01-01 18:39:50 +0900

    php修正

--- php/select.php
+++ php/select.php
@@ -37,8 +37,8 @@ if ($status === false) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ポモドーロアプリ</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="..css/reset.css">
+    <link rel="stylesheet" href="..css/style.css">
     <style>
         div {
             padding: 10px;
