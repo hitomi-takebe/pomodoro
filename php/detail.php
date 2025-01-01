b575f75baf2a89856b6df95cb08fea5844c57a4f
hitomi-takebe <takebe.hitomi@gmail.com>
2025-01-01 23:20:48 +0900

    detail1

--- php/detail.php
+++ php/detail.php
@@ -29,6 +29,9 @@ if ($status === false) {
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/style.css">
     <style>
+        div{
+            margin: 0 auto;
+        }
         .submit {
             max-width: 300px;
             margin-bottom: 20px;
@@ -49,7 +52,6 @@ if ($status === false) {
     <form method="POST" action="update.php">
         <div class="jumbotron">
             <fieldset>
-                <legend>フリーアンケート</legend>
                 <label>todo：<input type="text" name="todo" value="<?= $result['todo'] ?>"></label><br>
                 <label>振り返り：<input type="text" name="ref" value="<?= $result['ref'] ?>"></label><br>
                 <label>次回：<input type="text" name="next" value="<?= $result['next'] ?>"></label><br>
